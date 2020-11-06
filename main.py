3a69c9f1188ecb78e988f7ac122fe6a2704bab41
Maris Galesloot <m.galesloot@student.ru.nl>
2020-11-06 17:36:49 +0100

    Small update

--- main.py
+++ main.py
@@ -3,7 +3,9 @@
 import argparse
 
 def main():
-	parser = argparse.ArgumentParser(prog="IR-CLI", description="TREC-COVID document ranker CLI")
+	parser = argparse.ArgumentParser(description="TREC-COVID document ranker CLI")
+	parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+	args = parser.parse_args()
 
 if __name__ == '__main__':
 	main()
