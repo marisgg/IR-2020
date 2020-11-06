44cd90ff4d845ca7e110cf5f0b9777020a091ee6
Maris Galesloot <m.galesloot@student.ru.nl>
2020-11-06 13:33:29 +0100

    Stub for CLI using argparse

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import argparse
+
+def main():
+	parser = argparse.ArgumentParser(prog="IR-CLI", description="TREC-COVID document ranker CLI")
+
+if __name__ == '__main__':
+	main()
