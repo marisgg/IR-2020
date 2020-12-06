9731ccf4752462b76284bbce0a39356c3dfc5c4c
Maris Galesloot <marisgalesloot@hotmail.com>
2020-12-06 22:52:43 +0100

    Move tutorial code to method and fix document retrieval for less than max hits

--- index_trec.py
+++ index_trec.py
@@ -5,62 +5,63 @@ import math
 
 index_reader = index.IndexReader('lucene-index-cord19-abstract-2020-07-16')
 
-# Access basic index statistics
-print(index_reader.stats())
-total_documents = index_reader.stats()['documents']
-print(f'total amount of documents: {total_documents}')
+def tutorial_code():
+    # Access basic index statistics
+    print(index_reader.stats())
+    total_documents = index_reader.stats()['documents']
+    print(f'total amount of documents: {total_documents}')
 
-#Iterate over index terms and access term statistics
-for term in itertools.islice(index_reader.terms(), 10):
-    print(f'{term.term} (df={term.df}, cf={term.cf})')
+    #Iterate over index terms and access term statistics
+    for term in itertools.islice(index_reader.terms(), 10):
+        print(f'{term.term} (df={term.df}, cf={term.cf})')
 
-term = 'cities'
+    term = 'cities'
 
-# Analyze the term.
-analyzed = index_reader.analyze(term)
-print(f'The analyzed form of "{term}" is "{analyzed[0]}"')
+    # Analyze the term.
+    analyzed = index_reader.analyze(term)
+    print(f'The analyzed form of "{term}" is "{analyzed[0]}"')
 
-# Skip term analysis:
-df, cf = index_reader.get_term_counts(analyzed[0], analyzer=None)
-print(f'term "{term}": df={df}, cf={cf}')
+    # Skip term analysis:
+    df, cf = index_reader.get_term_counts(analyzed[0], analyzer=None)
+    print(f'term "{term}": df={df}, cf={cf}')
 
-# Fetch and traverse postings for an unanalyzed term
-postings_list = index_reader.get_postings_list(term)
-for posting in postings_list:
-    #print(f'docid={posting.docid}, tf={posting.tf}, pos={posting.positions}')
-    pass
+    # Fetch and traverse postings for an unanalyzed term
+    postings_list = index_reader.get_postings_list(term)
+    for posting in postings_list:
+        #print(f'docid={posting.docid}, tf={posting.tf}, pos={posting.positions}')
+        pass
 
-# Fetch and traverse postings for an analyzed term:
-postings_list = index_reader.get_postings_list(analyzed[0], analyzer=None)
-for posting in postings_list:
-    #print(f'docid={posting.docid}, tf={posting.tf}, pos={posting.positions}')
-    pass
-
-
-# First find docids within the prebuild index
-searcher = SimpleSearcher('lucene-index-cord19-abstract-2020-07-16')
-hits = searcher.search('lung')
-# Print the first 10 hits:
-#for i in range(0, 10):
-#    print(f'{i+1:2} {hits[i].docid:15} {hits[i].score:.5f}')
-test_docid = hits[0].docid
-print(f'test docid: {test_docid}')
-
-# Fetch the document vector for a document
-doc_vector = index_reader.get_document_vector(test_docid)
-#print(f'Document vector: {doc_vector}') # dict where keys are the analyzed terms and values the term frequenies
+    # Fetch and traverse postings for an analyzed term:
+    postings_list = index_reader.get_postings_list(analyzed[0], analyzer=None)
+    for posting in postings_list:
+        #print(f'docid={posting.docid}, tf={posting.tf}, pos={posting.positions}')
+        pass
 
 
-# Compute the tf-idf representation
-tf = index_reader.get_document_vector(test_docid)
-df = {term: (index_reader.get_term_counts(term, analyzer=None))[0] for term in tf.keys()} 
+    # First find docids within the prebuild index
+    searcher = SimpleSearcher('lucene-index-cord19-abstract-2020-07-16')
+    hits = searcher.search('lung')
+    # Print the first 10 hits:
+    #for i in range(0, 10):
+    #    print(f'{i+1:2} {hits[i].docid:15} {hits[i].score:.5f}')
+    test_docid = hits[0].docid
+    print(f'test docid: {test_docid}')
+
+    # Fetch the document vector for a document
+    doc_vector = index_reader.get_document_vector(test_docid)
+    #print(f'Document vector: {doc_vector}') # dict where keys are the analyzed terms and values the term frequenies
+
+
+    # Compute the tf-idf representation
+    tf = index_reader.get_document_vector(test_docid)
+    df = {term: (index_reader.get_term_counts(term, analyzer=None))[0] for term in tf.keys()} 
 
 def get_docids(term, max_doc=10):
     # First find docids within the prebuild index
     searcher = SimpleSearcher('lucene-index-cord19-abstract-2020-07-16')
     hits = searcher.search(term)
     hits_docid = []
-    for i in range(max_doc):
+    for i in range(min(len(hits), max_doc)):
         hits_docid.append(hits[i].docid)
 
     return hits_docid
