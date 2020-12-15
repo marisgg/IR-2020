3beeb4d89682585acb621e6bed54169235fa8c01
Jorane Rogier <jorane_rogier@hotmail.com>
2020-12-15 17:27:00 +0100

    rocchio update

--- models.py
+++ models.py
@@ -1,63 +1,173 @@
-from pyserini import analysis, index
 import itertools
-from pyserini.search import SimpleSearcher
 import math
+from pyserini.search import SimpleSearcher
+import pandas as pd
+import collections
+from index_trec import Index
+import numpy as np
 
-index_reader = index.IndexReader('lucene-index-cord19-abstract-2020-07-16')
-searcher = SimpleSearcher('lucene-index-cord19-abstract-2020-07-16')
+class Models:
+
+    def __init__(self, index, searcher):
+        self.index_reader = index
+        self.searcher = searcher
+        self.N = self.index_reader.stats()['documents']
+
+    def get_n_of_words_in_docid(self, docid):
+        """ Hacky: Sum all term frequencies in document vector (thus no stopwords) """
+        return sum(self.index_reader.get_document_vector(docid).values())
+
+    """
+    Compute TF-IDF, which consists of the following two components:
+    1. Term frequency: measures the frequency of a word in a document, normalize.
+        tf(t,d) = count of t in d / number of words in d
+    2. Inverse document frequency: measures the informativeness of term t.
+        idf(t) = log(N / (df + 1)               (df = occurences of t in documents)
+
+    The resulting formula: tf-idf(t,d) = tf(t,d)*log(N/(df+1))
+
+    INPUT:      Dictionary, with for each file a sub-dictionary containing the title, abstract, and introduction.
+    OUTPUT:     
+    """
+
+    def tf_idf_term(self, term, docid) -> float:
+        tfs = self.index_reader.get_document_vector(docid)
+        if term in tfs:
+            tf = tfs[term]/self.get_n_of_words_in_docid(docid)
+            df = self.index_reader.get_term_counts(term, analyzer=None)[0]
+            return tf * math.log(self.N / (df + 1))
+        else:
+            return 0.0
+
+
+    def tf_idf_docid(self, docid) -> {}:
+        tfs = self.index_reader.get_document_vector(docid)
+        tf_idf = {}
+        for term, count in tfs.items():
+            df = self.index_reader.get_term_counts(term, analyzer=None)[0]
+            tf_idf[term] = count/self.get_n_of_words_in_docid(docid) * math.log(self.N / (df + 1)) # added total number of words in doc
+        return tf_idf
+
+    def bm25_term(self, term, docid) -> float:
+        return self.index_reader.compute_bm25_term_weight(docid, term, analyzer=None)
+
+    def bm25_docid(self, docid) -> {}:
+        """ get all terms in documents """
+        tfs = self.index_reader.get_document_vector(docid)
+        bm25_vector = {term: self.index_reader.compute_bm25_term_weight(docid, term, analyzer=None) for term in tfs.keys()}
+        return bm25_vector
+
+
+
+
+    def create_collection_list(self):
+        """
+        Create list of all analyzed terms in the collection.
+        """
+        c_list = []
+        for term in itertools.islice(self.index_reader.terms(), 0, None):
+            c_list.append(term.term)
+        return c_list
+
+
+    def create_complete_vector(self, doc_vector):
+        """
+        Each term is represented as a dimension to be able to compare the documents.
+        For this, also the terms that are not present in the document should be added to the document vector.
+        """
+        c_terms = self.create_collection_list()
+        complete_doc_vector = doc_vector
+        for term in c_terms:
+            # TODO: smoothing of terms?
+            if not term in doc_vector:
+                complete_doc_vector[term] = 0
+        complete_dict =  collections.OrderedDict(sorted(complete_doc_vector.items()))
+        complete_vector_list = complete_dict.values()
+        return complete_vector_list
+            
+
+    def create_query_vector(self, q):
+        query_vector = {}
+        for t in q:
+            #if not t in query_vector:
+            query_vector[t] = 1 # =/= q.count(t), since BOW uses binary representation of the occurence of a term
+        complete_query_vector = self.create_complete_vector(query_vector)
+        return complete_query_vector
+
+    
+    def get_relevance_docs(self, query_id):
+        """
+        Read file with relevance of part of the collection.
+        Relevancy equals to 0 is irrelevant, 1 is relevant, and 2 is highly relevant.
+        """
+        relevance_data = pd.read_csv("qrels-covid_d5_j0.5-5.txt", sep=" ", header=None)
+        relevance_data.columns = ["topic_id", "round_id", "cord_uid", "relevancy"]
+
+        relevance_data = relevance_data[relevance_data.relevancy >= 0] # File contains 2 rows with -1
+
+        relevant_docs = relevance_data[(relevance_data.topic_id == query_id) & (relevance_data.relevancy > 0)]
+        non_relevant_docs = relevance_data[relevance_data.topic_id == 0] # Only use positive feedback
+
+        return [relevant_docs.cord_uid, non_relevant_docs.cord_uid]
+    
+
+    def rocchio_algorithm(self, qid, q0):
+        doc_ids = self.get_relevance_docs(qid)
+        relevant_doc_ids = doc_ids[0]
+        non_relevant_doc_ids = doc_ids[1]
+
+        relevant_doc_vectors = []
+        for doc_id in relevant_doc_ids:
+            relevant_doc_vectors.append(self.create_complete_vector(self.tf_idf_docid(doc_id)))
+        
+        non_relevant_doc_vectors = []
+        for doc_id in non_relevant_doc_ids:
+            non_relevant_doc_vectors.append(self.create_complete_vector(self.tf_idf_docid(doc_id)))
+        
+        # Standard values
+        beta = 0.75
+        alpha = 1.0
+        gamma = 0.15
+
+        # calculate centroid of relevant documents
+        summed_relevant_vectors = []
+        for doc in relevant_doc_vectors: 
+            summed_relevant_vectors = np.add(summed_relevant_vectors, doc)
+        centroid_relevant_docs = 1/len(relevant_doc_ids) * summed_relevant_vectors
+        
+        # TODO: In practice, only use positive feedback (set gamma to 0) --> test this
+        # calculate centroid of non-relevant documents
+        summed_non_relevant_vectors = []
+        for doc in non_relevant_doc_vectors: 
+            summed_non_relevant_vectors = np.add(summed_relevant_vectors, doc)
+        centroid_non_relevant_docs = 1/len(non_relevant_doc_ids) * summed_non_relevant_vectors
+        
+        # rocchio algorithm    
+        q_mod = alpha * q0 + beta * centroid_relevant_docs - gamma * centroid_non_relevant_docs 
+        return q_mod
+
+    def rocchio_ranking(self, qid, q0):
+        trec_index = Index(self.index_reader, self.searcher)
+        all_docs = trec_index.get_docids()
+
+        q_mod = self.rocchio_algorithm(qid, q0)
+        
+        doc_scores = {}
+
+        count=0
+        # Rank documents using dot product as similarity function
+        for doc in all_docs:
+            similarity_score = np.dot(self.create_complete_vector(self.tf_idf_docid(doc)), q_mod)
+            doc_scores[doc] = similarity_score
+            print(f"Doc nr.:{count} - Score: {similarity_score}")
+            count += 1
+        
+        return doc_scores
 
 """
-Compute TF-IDF, which consists of the following two components:
-1. Term frequency: measures the frequency of a word in a document, normalize.
-    tf(t,d) = count of t in d / number of words in d
-2. Inverse document frequency: measures the informativeness of term t.
-    idf(t) = log(N / (df + 1)               (df = occurences of t in documents)
+# code to plot graph with the balance of relevancy per topic.
+import plotly.express as px
 
-The resulting formula: tf-idf(t,d) = tf(t,d)*log(N/(df+1))
-
-INPUT:		Dictionary, with for each file a sub-dictionary containing the title, abstract, and introduction.
-OUTPUT:		
+fig = px.histogram(relevance_data, x="topic_id", color = "relevancy")
+fig.show()
 """
-
-""" Hacky: Sum all term frequencies in document vector (thus no stopwords) """
-def get_n_of_words_in_docid(docid):
-    return sum(index_reader.get_document_vector(docid).values())
-
-
-def tf_idf_term(term, docid):
-    N = index_reader.stats()['documents']
-    tfs = index_reader.get_document_vector(docid)
-    if term in tfs:
-        tf = tfs[term]/get_n_of_words_in_docid(docid)
-        df = index_reader.get_term_counts(term, analyzer=None)[0]
-        return tf * math.log(N / (df + 1))
-    else: 
-        return 0
-
-
-def tf_idf_docid(docid):
-    N = index_reader.stats()['documents']
-
-    tfs = index_reader.get_document_vector(docid)
-    tf_idf = {}
-    for term, count in tfs.items():
-        df = index_reader.get_term_counts(term, analyzer=None)[0]
-        tf_idf[term] = count/get_n_of_words_in_docid(docid) * math.log(N / (df + 1)) # added total number of words in doc
-    return tf_idf
-
-def bm25_term(term, docid):
-    return index_reader.compute_bm25_term_weight(docid, term, analyzer=None)
-     
-def bm25_docid(docid):
-    # get all terms in documents
-    tfs = index_reader.get_document_vector(docid)
-    bm25_vector = {term: index_reader.compute_bm25_term_weight(docid, term, analyzer=None) for term in tfs.keys()}
-    return bm25_vector
-
-
-'''hits = searcher.search('corona')
-# Print the first 10 hits:
-for i in range(0, 10):
-    print(f'{i+1:2} {hits[i].docid:15} {hits[i].score:.5f}')
-test_docid = hits[0].docid
-print(f'test docid: {test_docid}')'''
--- trec_main.py
+++ trec_main.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 import argparse
-from timeit import Timer
-from process_data import process_data
-from preprocess_data_optimized import preprocessing
-import index_trec
-import output
+import itertools
 import json
 import xml.etree.ElementTree as ET
-import numpy as np
-import models
+import sys
+from pyserini.index import IndexReader
+from pyserini.search import SimpleSearcher
+from output import write_output
+from models import Models
+from index_trec import Index
+from progress.bar import Bar
+from pyserini.analysis import Analyzer, get_lucene_analyzer
+import pytrec_eval
 
 def parse_topics(topicsfilename):
     topics = {}
@@ -34,69 +37,115 @@ def read_json_topics(filename):
         topics = json.load(infile)
         return topics
 
-def score_query(query, model):
+def preprocess_query(query):
+    stop_words = ["a","about","after","all","also","always","am","an","and","any","are","at","be","been","being","but","by","came","can","cant","come","could","did","didnt","do","does","doesnt","doing","dont","else","for","from","get","give","goes","going","had","happen","has","have","having","how","i","if","ill","im","in","into","is","isnt","it","its","ive","just","keep","let","like","made","make","many","may","me","mean","more","most","much","no","not","now","of","only","or","our","really","say","see","some","something","take","tell","than","that","the","their","them","then","there","they","thing","this","to","try","up","us","use","used","uses","very","want","was","way","we","what","when","where","which","who","why","will","with","without","wont","you","your","youre"]
+    return [word for word in query.split() if word not in stop_words]
+
+def score_query(query, model, index_class, models_class, topic_id):
     doc_scores = {}
     docs = set()
-    scores = []
-
+    analyzer = Analyzer(get_lucene_analyzer())
+    query = analyzer.analyze(query)
     # TODO: Get documents in which percentage of query terms exist? 
     """
     Ik stel het volgende voor (zonder onderbouwing verder): als query > 3 woorden bevat, kijken we
     naar documenten waarin minimaal 2 woorden zitten? (50%)
     --> Iig moeten we hier iets voor bedenken denk ik.
     """
-    for term in query.split():
-        postings = index_trec.get_docids_from_postings(term)
-        docs |= postings
-        if(verbose):
-            print(term)
-            print(len(docs))
+    if model == "rocchio":
+            doc_scores = models_class.rocchio_ranking(topic_id, query) #, ordered_doc_scores.keys()[:100])
     
-    count = 0
-    for doc in list(docs):
-        score = 0
-        for term in query.split():
-            #score += index_trec.tf_idf_term(term, doc)
-            if model == "bm25":
-                score += models.bm25_term(term, doc)
-            elif model == "tf_idf":
-                score += models.tf_idf_term(term, doc)
-            else:
-                print("No model found")
-            count += 1
-            if(verbose and count % 1000 == 0):
-                print("Processed {0} scores out of {1}..".format(count, len(list(docs))*len(query.split())))
-        doc_scores[doc] = score
+    else:
+        for term in query:
+            docs = index_class.get_docids_from_postings(term, return_set = docs, debug=True)
+            if verbose:
+                print(term)
+                print(len(docs))
 
+        count = 0
+        if verbose:
+            print(query)
+            bar = Bar("Computing scores for query", max=(len(list(docs))*len(query))/1000)
+        for doc in list(docs):
+            score = 0
+            for term in query:
+                if model == "bm25":
+                    score += models_class.bm25_term(term, doc)
+                elif model == "tf_idf":
+                    score += models_class.tf_idf_term(term, doc)
+                else:
+                    print("No model found")
+                    sys.exit(1)
+                count += 1
+                if verbose and count % 1000 == 0:
+                    bar.next()
+            if score > 0:
+                doc_scores[doc] = score
+        if verbose:
+            bar.finish()
+
+
+    # TODO: Take the top 1000 for output writing
     ordered_doc_scores = dict(sorted(doc_scores.items(), key=lambda item: item[1]), reverse=True)
+
+    ## reranking of the ranked documents (Rocchio algorithm) ## top-k ?
     return ordered_doc_scores
 
+
+
+def pytrec_dictionary_entry(qid, docid, score):
+    """ Create dictionary entry to update the total run output for pytrec_eval """
+    return {
+                'q' + str(qid) : {
+                    str(docid) : score
+                }
+            }
+
+def pytrec_evaluation(runfile, qrelfile, measures = pytrec_eval.supported_measures):
+    """ run trec_eval with "measures" from the Python interface """
+    with open(runfile, "r") as ranking:
+        run = pytrec_eval.parse_run(ranking)
+    with open(qrelfile, "r") as qrel:
+        qrel = pytrec_eval.parse_qrel(qrel)
+
+    evaluator = pytrec_eval.RelevanceEvaluator(
+        qrel, measures)
+
+    return evaluator.evaluate(run)
 
 def main():
     parser = argparse.ArgumentParser(description="TREC-COVID document ranker CLI")
     parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true", default=False)
-    parser.add_argument('-query', default="covid symptoms")
     parser.add_argument("-j", "--json", help="generate json from topics list", action="store_true", default=False)
-    parser.add_argument("-n", "--n_queries", help="maximum number of queries to run", default=1)
+    parser.add_argument("-n", "--n_queries", help="maximum number of queries to run", type=int, default=1)
     parser.add_argument("-m", "--model", help="which model used in ranking", default="bm25")
     args = parser.parse_args()
-    query = args.query
     global verbose
     verbose = args.verbose
     model = args.model
+
+    index_reader = IndexReader('lucene-index-cord19-abstract-2020-07-16')
+    searcher = SimpleSearcher('lucene-index-cord19-abstract-2020-07-16')
+    models = Models(index_reader, searcher)
+    trec_index = Index(index_reader, searcher)
 
     if args.json:
         # only need to do this once, program small MD5 or something
         write_topics_to_json("topics-rnd5.xml")
 
     topics = read_json_topics("topics.json")
+    try:
+        with open("ranking.txt", 'w') as outfile:
+            for idx in range(1, min(args.n_queries+1, 50)):
+                for docid, score in score_query(topics[str(idx)]["query"], model, trec_index, models, idx).items():
+                    if docid == "reverse":
+                        continue
+                    outfile.write(write_output(idx, docid, -1, score, "testrun"))
+    finally:
+        outfile.close()
 
-    output.clear_output()
-    for idx in range(1, min(args.n_queries+1, 50)):
-        for docid, score in score_query(topics[str(idx)]["query"], model).items():
-            if(docid == "reverse"):
-                continue
-            output.write_output(idx, docid, -1, score, topics[str(idx)]["query"])
-  
+    results = pytrec_evaluation("ranking.txt", "qrels-covid_d5_j0.5-5.txt")
+    print(results)
+
 if __name__ == "__main__":
     main()
